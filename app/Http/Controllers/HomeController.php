88be0f76e6237ae502cc67535a21d638046e1707
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-19 18:15:53 +0200

    render da db tramite dd($movies) ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+
+use App\Movie;
 
 class HomeController extends Controller
 {
     public function index(){
-        $movies = config('movies');
+        
+        $movies = Movie::all();
+
+        dd($movies);
         return view('home', compact('movies'));
     }
 }
