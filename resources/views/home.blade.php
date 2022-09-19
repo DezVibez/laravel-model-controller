e740b618be0277b83acb72e9d7e1a018ebf57634
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-19 17:20:20 +0200

    creato foreach per render successivo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,33 @@
 @extends('layouts.main')
+
+@section('title', 'Home')
+
+
+
+
+@section('main-content')
+
+
+  @foreach($movies as $movie)
+
+    <div class="card">
+
+        <h1>
+            {{$movie->title}}
+        </h1>
+
+        <h2>
+            {{$movie->original_title}}
+        </h2>
+
+        <h3>
+            {{$movie->nationality}}
+        </h3>
+
+    </div>
+
+  @endforeach
+
+
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movies | @yeald('title')</title>
+    <title> Movies | @yield('title')</title>
 
     
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -15,7 +15,11 @@
 
 
 <main>
-    @yield('main-content')
+    <div class="container">
+        
+        @yield('main-content')
+
+    </div>
 </main>
 
 
